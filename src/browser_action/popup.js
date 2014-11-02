dccc379aa111bb5e3f7795d9fa6e7fda6c8183d4
Ben Williams <b.williams@nib.com.au>
2014-11-03 10:12:56 +1100

    allowing toggling of cookie for riskRating

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -3,12 +3,41 @@ document.addEventListener('DOMContentLoaded', function () {
   hello();
 });
 
+document.getElementById('set').onclick = function(){
+  chrome.cookies.set({ url: "https://dev.austhealth.com", name: "", value: "RiskRating"});
+  document.getElementById('refresh').className = "";
+};
+
+document.getElementById('remove').onclick = function(){
+  chrome.cookies.set({ url: "https://dev.austhealth.com", name: "", value: ""});
+  document.getElementById('refresh').className = "";
+};
+
 function hello() {
+
+  var listFeatures = "";
 
   chrome.runtime.sendMessage({
       greeting: "getFeatures"
     },
     function(response) {
-      document.getElementById("features").textContent = response.msg;
-    });
+      listFeatures = response.msg;
+
+      document.getElementById("features").textContent = listFeatures;
+
+      if(listFeatures === " ft-risk-rating"){
+        document.getElementById('set').className += " is-hidden";
+        document.getElementById('riskRatingOff').className += " is-hidden";
+
+
+      }
+      else {
+        document.getElementById('remove').className += " is-hidden";
+        document.getElementById('riskRatingOn').className += " is-hidden";
+      }
+
+
+
+  });
+
 }
